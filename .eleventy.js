734c3b7b2e02d3c8fedf3e6bfcdf2ad582595c11
Nuno Freire <greven.pt@gmail.com>
2020-10-06 13:21:09 +0100

    WIP on image optimization support

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const { DateTime } = require("luxon");
+const Image = require("@11ty/eleventy-img");
+const sharp = require("sharp");
 const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
@@ -17,10 +19,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true);
 
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
-
-  eleventyConfig.addShortcode("version", function () {
-    return String(Date.now());
-  });
 
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
@@ -69,6 +67,61 @@ module.exports = function (eleventyConfig) {
 
     // returning an array in addCollection works in Eleventy 0.5.3
     return [...tagSet];
+  });
+
+  eleventyConfig.addShortcode("version", function () {
+    return String(Date.now());
+  });
+
+  // Images optimization
+  // Usage: {% Image "/images/00.jpg", "this is an alt description" %}
+  eleventyConfig.addNunjucksAsyncShortcode("Image", async (src, alt) => {
+    if (!alt) {
+      throw new Error(`Missing \`alt\` on image from: ${src}`);
+    }
+
+    let stats = await Image(src, {
+      widths: [25, 320, 640, 960, 1200, 1800, 2400],
+      formats: ["jpeg", "webp"],
+      urlPath: "/images/",
+      outputDir: "./_site/images/",
+    });
+
+    let lowestSrc = stats["jpeg"][0];
+
+    const placeholder = await sharp(lowestSrc.outputPath)
+      .resize({ fit: sharp.fit.inside })
+      .blur()
+      .toBuffer();
+
+    const base64Placeholder = `data:image/png;base64,${placeholder.toString(
+      "base64"
+    )}`;
+
+    const srcset = Object.keys(stats).reduce(
+      (acc, format) => ({
+        ...acc,
+        [format]: stats[format].reduce(
+          (_acc, curr) => `${_acc} ${curr.srcset} ,`,
+          ""
+        ),
+      }),
+      {}
+    );
+
+    const source = `<source type="image/webp" data-srcset="${srcset["webp"]}" >`;
+
+    const img = `<img
+      class="lazy"
+      alt="${alt}"
+      src="${base64Placeholder}"
+      data-src="${lowestSrc.url}"
+      data-sizes='(min-width: 1024px) 1024px, 100vw'
+      data-srcset="${srcset["jpeg"]}"
+      width="${lowestSrc.width}"
+      height="${lowestSrc.height}">`;
+
+    return `<div class="image-wrapper"><picture> ${source} ${img} </picture></div>`;
   });
 
   // Build
