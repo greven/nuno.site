32f75e7cf6bcdc7a28e17383b2a03dd606916271
Nuno Freire <greven.pt@gmail.com>
2023-03-02 11:19:05 +0000

    Translations

--- mix.exs
+++ mix.exs
@@ -96,7 +96,8 @@ defmodule App.MixProject do
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
+      translate: ["gettext.extract", "gettext.merge priv/gettext"],
     ]
   end
 end
