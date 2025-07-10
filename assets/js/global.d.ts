1e4f32bb8dfd477b064e58d0c19d4e48664fd086
Nuno Freire <mail@nuno.site>
2025-07-10 01:18:11 +0100

    🚧 WIP

--- assets/js/global.d.ts
+++ assets/js/global.d.ts
@@ -3,6 +3,6 @@ import type { LiveSocket } from 'phoenix_live_view';
 declare global {
   interface Window {
     liveSocket?: LiveSocket;
-    liveReloadeliveSocketr?: unknown;
+    liveReloader?: unknown;
   }
 }
