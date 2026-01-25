09e60cb8b67b97d899bedf1bf6b27d37727f19ea
Nuno Freire <mail@nuno.site>
2026-01-25 02:41:54 +0000

    chore: bump version to v0.1.27

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Site.MixProject do
   def project do
     [
       app: :site,
-      version: "0.1.26",
+      version: "0.1.27",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
