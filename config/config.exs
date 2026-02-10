d86c5f99faff8b674ea7d96ddbcd6839e1eeb171
Nuno Freire <mail@nuno.site>
2026-02-10 10:02:14 +0000

    chore: deps

--- config/config.exs
+++ config/config.exs
@@ -45,7 +45,7 @@ config :site, SiteWeb.Endpoint,
 config :site, Site.Mailer, adapter: Swoosh.Adapters.Local
 
 config :bun,
-  version: "1.3.3",
+  version: "1.3.9",
   assets: [args: [], cd: Path.expand("../assets", __DIR__)],
   js: [
     args:
