ed61e3767db0fe722a94f5ba0dd922f62dd5e3f1
Nuno Freire <mail@nuno.site>
2026-06-16 11:08:12 +0100

    chore: typo

--- lib/site_web/live/photos_live/index.ex
+++ lib/site_web/live/photos_live/index.ex
@@ -1,6 +1,10 @@
 defmodule SiteWeb.PhotosLive.Index do
   use SiteWeb, :live_view
 
+  alias Site.Gallery
+
+  alias SiteWeb.PhotosLive.Components
+
   @impl true
   def render(assigns) do
     ~H"""
@@ -9,11 +13,46 @@ defmodule SiteWeb.PhotosLive.Index do
       current_scope={@current_scope}
       active_link={@active_link}
     >
-      <Layouts.page_content class="flex flex-col gap-16">
-        <h2 class="text-3xl font-semibold">🚧 Work in Progress</h2>
-        <p class="text-content-40">Coming soon...™️</p>
+      <Layouts.page_content class="flex flex-col gap-8">
+        <.header underlined>
+          Photos
+          <:subtitle>A collection of moments I've captured.</:subtitle>
+        </.header>
+
+        <div :if={@photos == []} class="text-center py-16 text-content-40">
+          <p>No photos yet.</p>
+        </div>
+
+        <Components.photo_grid :if={@photos != []} photos={@photos} />
+
+        <Components.lightbox photo={@selected_photo} show={@show_lightbox} />
       </Layouts.page_content>
     </Layouts.app>
     """
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    photos = Gallery.list_photos()
+
+    socket =
+      socket
+      |> assign(:page_title, "Photos")
+      |> assign(:photos, photos)
+      |> assign(:selected_photo, nil)
+      |> assign(:show_lightbox, false)
+
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_event("open-photo", %{"id" => id}, socket) do
+    photo = Gallery.get_photo(id)
+
+    {:noreply, assign(socket, selected_photo: photo, show_lightbox: true)}
+  end
+
+  def handle_event("close-photo", _params, socket) do
+    {:noreply, assign(socket, selected_photo: nil, show_lightbox: false)}
   end
 end
